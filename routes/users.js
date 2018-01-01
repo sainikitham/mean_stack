40290239ca70a7cda9d2f43914b09cf43ee04521
Mandavilli Nikitha <msainikitha@gmail.com>
2018-01-01 16:44:37 +0530

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -37,7 +37,7 @@ router.post('/authenticate',(req,res,next) => {
         });
         res.json({
           success:true,
-          token:'JWT'+token,
+          token:'JWT '+token,
           user:{
             id:user._id,
             name:user.name,
