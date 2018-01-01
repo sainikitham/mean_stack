a7b2c15a514c3d913043f93929aee64e762c15d1
Mandavilli Nikitha <msainikitha@gmail.com>
2018-01-01 16:42:40 +0530

    Update passport.js

--- config/passport.js
+++ config/passport.js
@@ -4,7 +4,7 @@ const User = require('../models/user');
 const config = require('../config/database');
 module.exports = function(passport){
 var opts = {}
-opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderWithScheme('jwt' );
 opts.secretOrKey = config.secret;
 passport.use(new JwtStrategy(opts, function(jwt_payload, done) {
     User.getUserById(jwt_payload._id, function(err, user) {
